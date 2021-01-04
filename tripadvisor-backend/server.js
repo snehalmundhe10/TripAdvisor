95948d53163de5b2ed82297178c9f74f8fe3a5ec
Snehal Mundhe <m.snehal106@gmail.com>
2021-01-04 17:30:02 -0500

    feat: implemented post endpoint

--- tripadvisor-backend/server.js
+++ tripadvisor-backend/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 // Get all restaurants
 app.get("/api/v1/restaurants", async (req,res) => {
     try {
-        const results = await db.query("select * from restaurants");
+        const results = await db.query("SELECT * FROM restaurants");
         console.log(results);
         res.status(200).json({
             status: "success",
@@ -32,31 +32,42 @@ app.get("/api/v1/restaurants", async (req,res) => {
 
 // Get a restaurant with id
 app.get("/api/v1/restaurants/:id", async (req,res) => {
-  try {
-    const results = 
-            await db.query("select * from restaurants where id = $1", [req.params.id]);  
-    res.status(200).json({
-        status: "success",
-        data: {
-            restaurant : results.rows[0]
-        }
-    });     
-  }
-  catch(err){
-    console.log(err);
-  }
+    try {
+        const results = 
+                await db.query("SELECT * FROM restaurants WHERE id = $1", [req.params.id]);  
+        res.status(200).json({
+            status: "success",
+            data: {
+                restaurant : results.rows[0]
+            }
+        });     
+    }
+    catch(err){
+        console.log(err);
+    }
 });
-//create a restaurant
-app.post("/api/v1/restaurants/", (req,res) => {
-    console.log(req.body);
-    res.status(201).json({
-        status: "success",
-        data:{
-            restaurant: "mcdonalds"
-        },
-    })
+
+//Create a restaurant
+app.post("/api/v1/restaurants/", async (req,res) => {
+    try {
+        const results = 
+           await db.query("INSERT INTO restaurants (name, location, price_range) values ($1, $2, $3) returning *"
+           , [req.body.name, req.body.location, req.body.price_range]);
+        console.log(results.body);
+        res.status(201).json({
+            status: "success",
+            data:{
+                restaurant: results.rows[0]
+            }
+        });
+    } 
+    catch(err){
+        console.log(err);
+    }
+    
 });
-//update restaurants
+
+//Update restaurants
 app.put("/api/v1/restaurants/:id", (req,res) => {
     console.log(req.params.id);
     console.log(req.body);
