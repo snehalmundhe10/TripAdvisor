46a5b32fab8c3fcf11b666670f9a855b6e7c52ad
Snehal Mundhe <m.snehal106@gmail.com>
2021-01-05 14:13:54 -0500

    feat:implemented axios for post

--- tripadvisor-client/src/components/AddRestaurant.jsx
+++ tripadvisor-client/src/components/AddRestaurant.jsx
@@ -1,19 +1,50 @@
-import React from 'react'
-import "./AddRestaurant.css"
+import React, {useState} from 'react';
+import RestaurantFinder from "../apis/RestaurantFinder";
+import "./AddRestaurant.css";
 
 const AddRestaurant = () => {
+    const [name, setName] = useState("");
+    const [location, setLocation] = useState("");
+    const [priceRange, setPriceRange] = useState("Price Range");
+   const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+          const response =  await RestaurantFinder.post("/", {
+                name: name,
+                location: location,
+                price_range: priceRange
+            });
+            console.log(response);
+        }catch(err){
+            
+        }
+    }
     return (
         <div className="mb-3">
             <form action="">
                 <div className="row g-3">
                     <div className="col-auto">
-                        <input type="text" className="form-control" placeholder="name" />
+                        <input
+                         value={name}
+                         onChange={ (e) => setName(e.target.value)}
+                         type="text" 
+                         className="form-control" 
+                         placeholder="name" />
                     </div>
                     <div className="col-auto">
-                        <input className="form-control" type="text" placeholder="location"/>
+                        <input 
+                        value={location}
+                        onChange={ (e) => setLocation(e.target.value)}
+                        className="form-control" 
+                        type="text" 
+                        placeholder="location"/>
                     </div>
                     <div className="col-auto">
-                        <select className="custom-select my-1 mr-sm-2">
+                        <select 
+                        value={priceRange}
+                        onChange={ (e) => setPriceRange(e.target.value)}
+                        className="custom-select my-1 mr-sm-2"
+                        >
                             <option disabled> Price </option>
                             <option value="1">$</option>
                             <option value="2">$$</option>
@@ -22,7 +53,12 @@ const AddRestaurant = () => {
                             <option value="5">$$$$$</option>
                         </select> 
                     </div>
-                    <button className="btn btn-primary">Add</button>
+                    <button 
+                    onClick={handleSubmit}
+                    type="submit"
+                    className="btn btn-primary">
+                        Add
+                    </button>
                 </div>   
             </form>
         </div>
