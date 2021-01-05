1eaebd14156877ba02f10567459acdf74e0c0902
Snehal Mundhe <m.snehal106@gmail.com>
2021-01-04 19:22:00 -0500

    feat: added routes

--- tripadvisor-client/src/App.jsx
+++ tripadvisor-client/src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './routes/Home';
+import RestaurantDetailPage from './routes/RestaurantDetailPage';
+import UpdatePage from './routes/UpdatePage';
+
 const App =  () => {
     return (
       <div>
-          App
+          <Router>
+            <Switch>
+                <Route exact path="/" component={Home}/>
+                <Route exact path="/restaurants/:id/update" component={UpdatePage}/>
+                <Route exact path="/restaurants/:id" component={RestaurantDetailPage}/>
+            </Switch>
+          </Router>
+        
+
+
       </div>
     );
 }
