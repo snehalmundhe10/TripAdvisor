3c9b73a5e6afc9d598d1d9302e87d8437b61657c
Snehal Mundhe <m.snehal106@gmail.com>
2021-01-06 06:16:08 -0500

    feat:added route for reviews

--- tripadvisor-backend/server.js
+++ tripadvisor-backend/server.js
@@ -108,6 +108,22 @@ app.delete("/api/v1/restaurants/:id", async (req,res) => {
 
 });
 
+app.post("/api/v1/restaurants/:id/addReview", async (req,res) =>{
+    try {
+        const newReviews = 
+            await db.query("INSERT INTO reviews (restaurant_id, name, review, rating) values($1, $2, $3, $4) returning *;",
+            [req.params.id, req.body.name, req.body.review, req.body.rating]);
+        
+        res.status(201).json({
+            status: "success",
+            data: {
+                review: newReviews.rows[0]
+            }
+        });
+    }catch(err){
+        console.log(err);
+    }
+})
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
