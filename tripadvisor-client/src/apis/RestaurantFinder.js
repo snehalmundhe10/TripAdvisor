c1cd0cebbf6ded5b5608d860c78730e017b0223b
Snehal Mundhe <m.snehal106@gmail.com>
2021-01-07 08:41:12 -0500

    fix: changed axios for production

--- tripadvisor-client/src/apis/RestaurantFinder.js
+++ tripadvisor-client/src/apis/RestaurantFinder.js
@@ -1,5 +1,7 @@
 import axios from "axios";
-// initializing axios instance
+
+const baseURL = process.env.NODE_ENV === 'production' ? "api/v1/restaurants" 
+: "http://localhost:4000/api/v1/restaurants"
 export default axios.create({
-    baseURL: "http://localhost:4000/api/v1/restaurants"
+    baseURL,
 })
